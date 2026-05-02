b2f64c3554776bef083fbd208e81076eea883d7b
David Y. Stephenson <david@davidystephenson.com>
2026-05-02 12:16:27 -0400

    Render condtionally with ternary

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import "./App.css"
 import Register from "./pages/Auth/Register";
 import Login from "./pages/Auth/Login";
 import { useState } from "react";
-import Content from "./Content";
 
 function App() {
   const [currentPage, setCurrentPage] = useState('register');
-
   return (
     <div className="app-layout">
-      <Content currentPage={currentPage} />
+      {currentPage === 'register' ? <Register /> : <Login />}
     </div>
   );
 }
